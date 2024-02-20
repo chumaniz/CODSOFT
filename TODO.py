95bd2e1accc18c431aeaf992a36aa122f2eaf36a
chumaniz <131617454+chumaniz@users.noreply.github.com>
2024-02-20 13:07:58 +0200

    Creating window and title

--- TODO.py
+++ TODO.py
@@ -1,1 +1,12 @@
 # I am importing the Tkinter Library
+# What I did was import a GUI Library with which I can design my application
+# As well the messagebox widget which displays messages
+from tkinter import *
+import tkinter.messagebox 
+
+window = Tk()
+# Now we proceed to give a title
+window.title("Amahle's Todolist APP")
+window.mainloop()
+# Tk() creates application window
+# mainloop() runs Tkinter event loop and displays everything
