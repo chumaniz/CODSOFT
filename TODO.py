eb6c914efd57e7d33060bdbf790772a603a85534
chumaniz <131617454+chumaniz@users.noreply.github.com>
2024-02-20 13:41:54 +0200

    Frame Widget for listbox/scrollbar, created frame widget and Listbox for holding list items

--- TODO.py
+++ TODO.py
@@ -3,10 +3,26 @@
 # As well the messagebox widget which displays messages
 from tkinter import *
 import tkinter.messagebox 
+import tkinter as tk
 
 window = Tk()
 # Now we proceed to give a title
 window.title("Amahle's Todolist APP")
+
+# Creating Frame widget to hold the listbox and scrollbar 
+frameTask = Frame(window)
+frameTask.pack()
+
+# Holding items in a list box
+listboxTask=Listbox(frameTask, bg="green", fg="red", height=20, width=60, font="Modern")
+listboxTask.pack(side=tk.RIGHT)
+
+
+
+
+
+
+
 window.mainloop()
-# Tk() creates application window
 # mainloop() runs Tkinter event loop and displays everything
+
