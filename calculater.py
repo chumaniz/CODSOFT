0fbee2f36b91c67c4652c5762f4c2b8df13d6513
chumaniz <131617454+chumaniz@users.noreply.github.com>
2024-02-21 17:24:47 +0200

    Added a clear button function

--- calculater.py
+++ calculater.py
@@ -6,10 +6,17 @@ window.geometry('300x250')
 window.resizable(0,0)
 window.title("Calculator")
 
+# This function takes button presses and converts them to the screen respectively
 def button_click(item):
     global expression
     expression = expression + str(item)
     inputText.set(expression)
+
+# This function enables the clearing of the screen
+def button_clear():
+    global expression
+    expression = ""
+    inputText.set("")
 
 
 
